aefe41665808a8875891f7e81391173f8e31353c
itsDevbrat <debabrata.d@somaiya.edu>
2020-05-24 10:49:54 +0530

    Add students done /api/student

--- src/api/student/student.controller.js
+++ src/api/student/student.controller.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict"
 const connection = require('../../config/db')
 
 module.exports = {
@@ -29,10 +29,32 @@ module.exports = {
         return new Promise((resolve, reject) => {
             connection.execute('UPDATE reports SET face_suspicion = ?, eye_suspicion = ?, tab_switches = ? WHERE student_email = ? AND test_id = ?', [faceSuspicion, eyeSuspicion, tabSwitches, email, testId], (err, result) => {
                 if (err)
-                reject(err)
-            else
-                resolve({ msg: "UPDATED" })
+                    reject(err)
+                else
+                    resolve({ msg: "UPDATED" })
+            })
         })
-    })
-}
+    },
+    insert: async ({ name, email, filename }) => {
+        return new Promise((resolve, reject) => {
+            connection.execute('INSERT INTO students VALUES(?,?,?)', [email, name, "http://localhost:3000/uploads/"+filename ], (err, result) => {
+                if (err)
+                    reject(err)
+                else
+                    resolve({ email: email, name: name, path: "http://localhost:3000/uploads/" + filename})
+            })
+        })
+    },
+    fetch : async ({ email }) => {
+        return new Promise((resolve, reject) => {
+            connection.execute('SELECT * FROM students WHERE email = ?', [email], (err, rows, fields) => {
+                if (err)
+                    reject(err)
+                else if (rows[0] == null)
+                    reject(err)
+                else
+                    resolve(rows[0])
+            })
+        })
+    }
 }
--- src/api/student/student.router.js
+++ src/api/student/student.router.js
@@ -1,9 +1,40 @@
 'use strict';
 const studentRouter = require('express').Router()
-    , studentController = require('./student.controller');
+    , studentController = require('./student.controller')
+    , path = require('path')
+    , multer = require('multer');
+
+let storage = multer.diskStorage({
+    destination: path.join(__dirname, '/../../../public/uploads'),
+    filename: (req, file, callback) => {
+        callback(null, Date.now() + file.originalname);
+    }
+});
+
+let upload = multer({ storage: storage })
+
 
 studentRouter
-    .get('/all', async (req, res) => { 
+    .get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../../public/html/create_student.html'))
+    })
+    .get('/:email', async (req, res) => {
+        try {
+            const result = await studentController.fetch({ email: req.params.email })
+            res.json(result)
+        } catch (error) {
+            res.sendStatus(404)
+        }
+    })
+    .post('/', upload.single('image'), async (req, res) => {
+        try {
+            const result = await studentController.insert({ name: req.body.name, email: req.body.email, filename: req.file.filename })
+            res.json(result)
+        } catch (error) {
+            res.sendStatus(404)
+        }
+    })
+    .get('/all', async (req, res) => {
         try {
             const result = await studentController.getAll()
             res.json(result)
@@ -13,7 +44,7 @@ studentRouter
     })
     .get('/report/:email', async (req, res) => {
         try {
-            const result = await studentController.getReport({email: req.params.email})
+            const result = await studentController.getReport({ email: req.params.email })
             res.json(result)
         } catch (error) {
             res.sendStatus(404)
@@ -22,7 +53,7 @@ studentRouter
     .put('/report/:testid', async (req, res) => {
         console.log(req.body)
         try {
-            const result = await studentController.updateReport({ faceSuspicion: req.body.faceSuspicion, eyeSuspicion: req.body.eyeSuspicion, tabSwitches: req.body.tabSwitches, email: req.body.studentEmail, testId : req.params.testid })
+            const result = await studentController.updateReport({ faceSuspicion: req.body.faceSuspicion, eyeSuspicion: req.body.eyeSuspicion, tabSwitches: req.body.tabSwitches, email: req.body.studentEmail, testId: req.params.testid })
             res.json(result)
         } catch (error) {
             res.sendStatus(404)
